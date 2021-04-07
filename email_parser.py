9be90f60e6d08aad89f985bc23d6bef4af39851e
Decagon <decagon@Tosins-MacBook-Air.local>
2021-04-07 14:41:01 +0100

    complete function email_parser to return dictionary if email provided follows constraints

--- email_parser.py
+++ email_parser.py
@@ -2,3 +2,14 @@ import re
 def email_parser(email):
   pattern = re.compile(r'[a-z][a-zA-Z0-9+]+[a-zA-Z0-9]@[a-z][a-zA-Z0-9]+\.com', re.IGNORECASE)
   match = pattern.findall(email)
+  if email in match:
+    split_index = email.index('@')
+    username = email[:split_index]
+    domain  = email[split_index+1:]
+    return {
+      'username': username, 
+      'domain' : domain
+      }
+
+  else:
+    return None
